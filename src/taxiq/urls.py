a1cd59c851821b20f30198e865867beca0c9fea2
root <root@Attito>
2023-10-04 11:46:57 +0000

    settings tailwind ok

--- src/taxiq/urls.py
+++ src/taxiq/urls.py
@@ -17,8 +17,10 @@ Including another URLconf
 from django.conf import settings
 from django.contrib import admin
 from django.urls import path
+from . import views
 
 urlpatterns = [
+    path('',views.home, name='home'),
     path('admin/', admin.site.urls),
 ]
 
